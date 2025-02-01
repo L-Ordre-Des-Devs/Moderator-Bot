67d1f0f6af5aacabc42c8c7b06cee9d5a1bf8dce
Grievack <warriorofmeadow@gmail.com>
2025-02-01 01:20:57 +0100

    Merge ticket code with base code

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,37 +2,56 @@ const client = require("../index");
 const { GatewayIntentBits, PermissionFlagsBits, Client } = require("discord.js");
 
 client.on("messageCreate", async (message) => {
+    const { console, prisma } = client;
+
+    // Token in message manager
     const tokens = message.content.match(/[a-z,0-9,\_,\-]{24}\.[a-z,0-9,\_,\-]{6}\.[a-z,0-9,\_,\-]{38}/gmi);
-    if (tokens === null)
-        return;
-
-    client.console.warn("Tokens found:", tokens);
-    tokens.forEach(token => {
-        let tmpclient = new Client({
-            intents: [
-                GatewayIntentBits.Guilds
-            ]
-        });
-        
-        tmpclient.login(token).then(_ => {
-            client.console.log(`Token valid: ${token}`);
-        }).catch(err => {
-            client.console.error(`Token invalid: ${token}`);
-        });
-
-        tmpclient.on("ready", () => {
-            tmpclient.guilds.cache.each(async guild => {
-                let chan = await guild.channels.cache.find(ch => {
-                    return ch.isTextBased &&
-                        ch.isTextBased() &&
-                        ch.permissionsFor(guild.members.me).has(PermissionFlagsBits.SendMessages);
-                });
-                if (!chan) {
-                    return;
-                }
-                await chan.send("Ne publie pas tes tokens n'importe où !!! Un token est comme un mot de passe, ça ne se partage pas. Pour en apprendre plus sur la sécurité informatique -> https://discord.com/servers/l-ordre-des-devs-706640777450881114").catch(_ => {});
+    if (tokens !== null) {
+        client.console.warn("Tokens found:", tokens);
+        tokens.forEach(token => {
+            let tmpclient = new Client({
+                intents: [
+                    GatewayIntentBits.Guilds
+                ]
             });
-            tmpclient.destroy();
+
+            tmpclient.login(token).then(_ => {
+                console.log(`Token valid: ${token}`);
+            }).catch(err => {
+                console.error(`Token invalid: ${token}`);
+            });
+
+            tmpclient.on("ready", () => {
+                tmpclient.guilds.cache.each(async guild => {
+                    let chan = await guild.channels.cache.find(ch => {
+                        return ch.isTextBased &&
+                            ch.isTextBased() &&
+                            ch.permissionsFor(guild.members.me).has(PermissionFlagsBits.SendMessages);
+                    });
+                    if (!chan) {
+                        return;
+                    }
+                    await chan.send("Ne publie pas tes tokens n'importe où !!! Un token est comme un mot de passe, ça ne se partage pas. Pour en apprendre plus sur la sécurité informatique -> https://discord.com/servers/l-ordre-des-devs-706640777450881114").catch(_ => { });
+                });
+                tmpclient.destroy();
+            });
         });
-    });
+        return;
+    }
+
+    // Tickets manager
+    if (message.channel?.name?.startsWith('ticket-')) {
+        const channelId = message.channel.id;
+        const ticket = await prisma.ticket.findUnique({
+            where: { channelId }
+        });
+
+        if (ticket) {
+            await prisma.ticket.update({
+                where: { id: ticket.id },
+                data: { lastActivity: new Date() }
+            });
+        }
+    }
+
 });
