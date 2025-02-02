fade4fc9e31a1cdbcb7daf72e665b93deae074a5
Mizari-Dev <warriorofmeadow@gmail.com>
2025-02-02 16:27:43 +0100

    clean guildMemberUpdate

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,36 +1,43 @@
 const client = require("../index");
+const { console } = client;
 
 client.on("guildMemberUpdate", async (oldMember, newMember) => {
     // si l'ancien pending est différent du nouveau que le nouveau est false
-	if(oldMember.pending !== newMember.pending && !newMember.pending){
-        const { console } = client;
-
-        // récupération du serv
-    	const member = newMember;
-      	const { guild } = member;
-  		if (guild.id !== process.env.GUILD_ID)
-    		return;
-
-  		var channel = process.env.WELCOME_CHNL;
-  		var msg = process.env.WELCOME_MSG;
-
-        msg = msg.replaceAll("{server.name}", guild.name);
-        msg = msg.replaceAll("{server.description}", guild.description);
-        msg = msg.replaceAll("{member.name}", member.user.username);
-        msg = msg.replaceAll("{member.tag}", member.user.tag);
-        msg = msg.replaceAll("{member}", member);
-        msg = msg.replaceAll("\\n", String.fromCharCode(10));
-
-        guild.channels.cache.get(channel).send(msg);
-
-        let role = process.env.WELCOME_ROLE;
-        role = guild.roles.cache.get(role);
-        member.roles.add(role).catch(err => console.error(err));
-
-        let content = {
-            message: "New member",
-            member: newMember.username,
-        };
-        console.table(content);
+    if (oldMember.pending !== newMember.pending && !newMember.pending) {
+        Welcome(newMember);
     }
 });
+
+/**
+ * Add role and send message to welcome new member
+ * @param {GuildMember} member The member
+ * @returns {void} void
+ */
+function Welcome(member) {
+    // récupération du serv
+    const { guild } = member;
+    if (guild.id !== process.env.GUILD_ID)
+        return;
+
+    var channel = process.env.WELCOME_CHNL;
+    var msg = process.env.WELCOME_MSG;
+
+    msg = msg.replaceAll("{server.name}", guild.name);
+    msg = msg.replaceAll("{server.description}", guild.description);
+    msg = msg.replaceAll("{member.name}", member.user.username);
+    msg = msg.replaceAll("{member.tag}", member.user.tag);
+    msg = msg.replaceAll("{member}", member);
+    msg = msg.replaceAll("\\n", String.fromCharCode(10));
+
+    guild.channels.cache.get(channel).send(msg);
+
+    let role = process.env.WELCOME_ROLE;
+    role = guild.roles.cache.get(role);
+    member.roles.add(role).catch(err => console.error(err));
+
+    let content = {
+        message: "New member",
+        member: member.username,
+    };
+    console.table(content);
+}
